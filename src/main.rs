1f06709e731682cb7eaa5453b2d401260169565f
Eric Myhre <hash@exultant.us>
2024-10-20 18:03:06 +0200

    figuring out how to get subranges of actual values back out.  treesitter doesn't seem to have a helper to do this itself.  fair.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tree_sitter::{InputEdit, Language, Parser, Point};
+use tree_sitter::{InputEdit, Language, Parser, Point, TreeCursor};
 
 fn main() {
     println!("Hello, world!");
@@ -11,5 +11,25 @@ fn main() {
     let source_code = "type Whee string";
     let mut tree = parser.parse(source_code, None).unwrap();
     let root_node = tree.root_node();
-    println!("wowsa: {}", root_node)
+    println!("wowsa: {}", root_node);
+
+    myprint(source_code, root_node.walk());
+}
+
+fn myprint(doc: impl AsRef<[u8]>, tc: TreeCursor) {
+    let n = tc.node();
+    println!("{}", n.kind());
+    // TODO: some kind of condition like finding labelled nodes would be good here.  or leaf nodes?
+    // or just print an elided-middle fixed size and do so unconditionally.  unconditional can be good.
+    if n.is_named() {
+        println!(
+            "val: {:?}",
+            n.utf8_text(
+                doc.as_ref()
+                    .get(n.start_byte()..n.end_byte())
+                    .expect("valid range")
+            )
+            .expect("utf8")
+        )
+    }
 }
