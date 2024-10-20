d8e5a473b29f8cfab726f6af5dcdd6d239a0c360
Eric Myhre <hash@exultant.us>
2024-10-20 18:35:23 +0200

    add recursive walk.  fix getting of source text (tree-sitter does more than i thought).
    the cursor api is certainly... interesting... but it works.

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,29 @@ fn main() {
     let root_node = tree.root_node();
     println!("wowsa: {}", root_node);
 
-    myprint(source_code, root_node.walk());
+    myprint(&source_code, &mut root_node.walk());
 }
 
-fn myprint(doc: impl AsRef<[u8]>, tc: TreeCursor) {
+fn myprint(doc: &impl AsRef<[u8]>, tc: &mut TreeCursor) {
     let n = tc.node();
     println!("{}", n.kind());
+
     // TODO: some kind of condition like finding labelled nodes would be good here.  or leaf nodes?
     // or just print an elided-middle fixed size and do so unconditionally.  unconditional can be good.
     if n.is_named() {
-        println!(
-            "val: {:?}",
-            n.utf8_text(
-                doc.as_ref()
-                    .get(n.start_byte()..n.end_byte())
-                    .expect("valid range")
-            )
-            .expect("utf8")
-        )
+        println!("  val: {:?}", n.utf8_text(doc.as_ref()).expect("utf8"))
+    }
+
+    // TODO: print more things, like any field names.
+    // It seems really odd to me that you can't get a field name except by `n.field_name_for_child(child_index)` on the *parent*.
+    // Ah.  Do it on the cursor.  Ok lol.
+
+    // Walk children.
+    if !tc.goto_first_child() {
+        return;
+    }
+    myprint(doc, tc);
+    while tc.goto_next_sibling() {
+        myprint(doc, tc)
     }
 }
